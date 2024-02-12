7dece07871144a45c37fc54fd74d0f62b6211367
ahmdpt101 <ahmdpt101@gmail.com>
2024-02-13 01:19:05 +0300

    new out

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -19,7 +19,7 @@ export default function Contact() {
                     className='absolute left-[-10px] z-5 top-[-30px] w-10 lg:w-[50px]'
                 />
                 <h1 className='bold-52 z-0 lg:bold-50'>Contact Us</h1>
-                <p className='regular-16 mt-6 text-gray-30 xl:max-w-[520px]'>Need assistance or have a question? Reach out to us! Simply fill out the form below, and we'll get back to you ASAP. You can also call us at <span className="bold-20 text-red-500 underline">555 22 33</span> during business hours. Thanks for choosing us for your pizza fix!</p>
+                <p className='regular-16 mt-6 text-gray-30 xl:max-w-[520px]'>Need assistance or have a question? Reach out to us! Simply fill out the form below, and we`&apos;`ll get back to you ASAP. You can also call us at <span className="bold-20 text-red-500 underline">555 22 33</span> during business hours. Thanks for choosing us for your pizza fix!</p>
             </div>
             <div className="relative  flex flex-1 items-start">
                 <form action="" className="relative z-20 flex w-[468px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
